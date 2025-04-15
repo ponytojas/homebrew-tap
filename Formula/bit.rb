a7ed3bbaa8381dad06a9ec26a6e9437caf784730
Daniel Villalobos <daniel.villalobos@lumibit.io>
2025-04-15 07:57:33 +0200

    Update bit.rb

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Bit < Formula
   desc "CLI tool to automate file mongoose, graphql and apollo hooks based on a typescript interface"
   homepage "https://atc-github.azure.cloud.bmw/qxz5hea/bit-cli"
-  url "git@atc-github.azure.cloud.bmw:qxz5hea/bit-cli.git",
+  url "ssh://git@atc-github.azure.cloud.bmw/qxz5hea/bit-cli.git",
       tag:      "v2.0.0",
       revision: "b755b2d3e6931ea2bea8acce8d7a38f1c82f3d12"
   license "MIT"
